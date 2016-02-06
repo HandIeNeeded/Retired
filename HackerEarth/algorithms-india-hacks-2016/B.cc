09ce5137631d07490c44fa0122ed6321f550534c
yuzhou627 <yuzhou627@gmail.com>
2016-02-06 17:21:43 +0800

    add B

---
+++ HackerEarth/algorithms-india-hacks-2016/B.cc
@@ -0,0 +1,24 @@
+/*************************************************************************
+	> File Name: B.cc
+	> Author: yuzhou627
+	> Mail:   yuzhou627@gmail.com
+	> created Time: Sat 06 Feb 2016 05:21:16 PM CST
+ ************************************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+#define REP(i, a) REPP(i, 0, (a) - 1)
+#define REPP(i, a, b) for (int i = int(a); i <= int(b); i++)
+
+int main() {
+#ifdef HOME
+    string file(__FILE__);
+    freopen((file.substr(0, file.find('.')) + ".in").c_str(), "r", stdin);
+#endif
+
+
+
+    return 0;
+}
+
