580f123880cdd04cd2b6f2bd3f834d5a3233376b
yuzhou627 <yuzhou627@gmail.com>
2016-08-17 21:52:38 +0800

    [Retired::Library] refine merge sort

--- Library/C++/MergeSort.cc
+++ Library/C++/MergeSort.cc
@@ -1,23 +1,10 @@
-int a[N], b[N]; //b[N] -> tmp a[N] -> data
-
-LL Merge(int l,int r) {
-  if(l >= r) return 0; 
-  int mid = l + r >> 1;
-  Merge(l, mid);
-  Merge(mid + 1, r);
-
-  for (int i = l, j = mid + 1, k = l; k <= r; k++) {
-    if (j > r || i <= mid && a[i] < a[j]) {
-      b[k] = a[i++];
-    }
-    else if (j > r || i <= mid && a[i] == a[j]) {
-      b[k] = a[i++];
-    }
-    else {
-      b[k] = a[j++];
-    }
+template<class T>
+void MergeSort(T first, T last) {
+  if (last - first > 1) {
+    auto middle = first + (last - first) / 2;
+    MergeSort(first, middle);
+    MergeSort(middle, last);
+    std::inplace_merge(first, middle, last);
   }
-
-  REPP(i, l, r) a[i] = b[i];
-  return 0;
 }
+
